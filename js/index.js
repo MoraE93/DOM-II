534d5c39c237946faa4b1b7461443a76b2e84ea0
MoraE93 <chinexican97@gmail.com>
2021-03-16 21:26:04 -0400

    .

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const link = document.querySelectorAll('.nav-link')
+const navigation = document.querySelector('.main-navigation')
+const para = document.querySelectorAll('p')
+const images = document.querySelectorAll('img')
+const titles = document.querySelectorAll('h2')
+const buttons = document.querySelectorAll('.btn')
+
+
+
+let highlight = document.querySelector('destination');
+highlight.addEventListener('hover', (event) => {event.target.style.backgroundColor = "crimson"; })
